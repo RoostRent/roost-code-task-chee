2643ee8438027568b70bc2b2e5ca8a92a81cdefe
Chee <c.diep@hotmail.com>
2024-09-05 17:28:49 +0100

    Add tests for splitMoneyByRatio

--- src/money-helpers.test.ts
+++ src/money-helpers.test.ts
@@ -1,4 +1,4 @@
-import { splitMoneyEvenly } from './money-helpers';
+import { splitMoneyByRatio, splitMoneyEvenly } from './money-helpers';
 
 describe('money helpers', () => {
   describe('splitMoneyEvenly', () => {
@@ -21,5 +21,34 @@ describe('money helpers', () => {
         expect(splitMoneyEvenly(0, 2))
       }).toThrowError('Amount must be a positive number');
     });
+  });
+
+  describe('splitMoneyByRatio', () => {
+    it.each`
+      amount      | ratios            | output
+      ${12}       | ${[3, 2, 1]}      | ${[6, 4, 2]}
+      ${12}       | ${[2, 3, 1]}      | ${[4, 6, 2]}
+      ${14}       | ${[1, 3, 1, 1]}   | ${[3, 7, 2, 2]}
+      ${100}      | ${[1]}            | ${[100]}
+    `('splits $amount into the correct amounts based on ratios: $ratios', ({amount, ratios, output}) => {
+      expect(splitMoneyByRatio(amount, ratios)).toEqual(output);
+    })
+
+    it('throws an error if amount cannot be split', () => {
+
+      expect(() => {
+        expect(splitMoneyByRatio(0, [1, 2]))
+      }).toThrowError('Amount must be a positive number');
+    });
+
+    it('throws an error if ratios are not valid', () => {
+      expect(() => {
+        splitMoneyByRatio(100, []);
+      }).toThrow('Ratios must contain only positive numbers');
+
+      expect(() => {
+        splitMoneyByRatio(100, [1, 0, 2]);
+      }).toThrow('Ratios must contain only positive numbers');
+    })
   });
 });
