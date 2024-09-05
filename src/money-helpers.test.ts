ed8b1f73b2949c532386269a671aa4b05cd4442c
Chee <c.diep@hotmail.com>
2024-09-05 16:54:08 +0100

    Add tests for splitMoneyEvenly function

---
+++ src/money-helpers.test.ts
@@ -0,0 +1,25 @@
+import { splitMoneyEvenly } from './money-helpers';
+
+describe('money helpers', () => {
+  describe('splitMoneyEvenly', () => {
+    it.each`
+      amount      | split     | output
+      ${12}       | ${4}      | ${[3, 3, 3, 3]}
+      ${14}       | ${4}      | ${[4, 4, 3, 3]}
+      ${15}       | ${4}      | ${[4, 4, 4, 3]}
+      ${100}      | ${1}      | ${[100]}
+    `('splits $amount into $split equal groups', ({amount, split, output}) => {
+      expect(splitMoneyEvenly(amount, split)).toEqual(output);
+    })
+
+    it('throws an error if amount cannot be split', () => {
+      expect(() => {
+        splitMoneyEvenly(100, 0);
+      }).toThrow('Split must be a positive number');
+
+      expect(() => {
+        expect(splitMoneyEvenly(0, 2))
+      }).toThrowError('Amount must be a positive number');
+    });
+  });
+});
